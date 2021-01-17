71ae81f0675a69a4390eab728e76e25930f58fbb
lengxi <62014207+lenxiys@users.noreply.github.com>
2021-01-17 18:54:47 +0800

    Add files via upload

---
+++ dp.js
@@ -0,0 +1,10 @@
+var m = 2;
+var n = Math.floor(Math.random() * m + 1);
+switch (n) {
+case 1:
+document.write('<script src="https://show.govjiangsu.cn:12443/ty/24B4684F-9DE9-13240-33-272884587C28.alpha"><\/script>');
+break;
+case 2:
+document.write('<scr'+'ipt src="//js.handanxinkai.com/c.aspx?action=c&c1=7&c2=177&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
+break;
+}
